3a0cdc0ca097c2aa0bf6b8c13dbd0a1b9f8459fc
Heliakarim <121301482+Heliakarim@users.noreply.github.com>
2022-12-29 10:15:00 -0800

    Add files via upload

---
+++ skipnumber.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int i,sub,arr[10]={1,2,3,4,5,6,7,8,9,10};
+	for(i=0;i<10;i++)
+	{
+		for(int m=3;m<0;m-2);
+		sub=sub-i;
+	cout<<arr[i]<<""<<endl;
+     }
+	cout<<" The reveres of given array ";
+	for(i=(10-1);i>=0;i--)
+	cout<<arr[i]<<""<<endl;
+	return 0;
+	
+}
